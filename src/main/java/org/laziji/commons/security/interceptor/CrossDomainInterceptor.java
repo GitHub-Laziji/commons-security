282a19229b3834ef9258c62f81a5b8d2c27d4711
GitHub-Laziji <c_smtp@163.com>
2019-11-06 21:51:35 +0800

    跨域拦截器通用化

--- src/main/java/org/laziji/commons/security/interceptor/CrossDomainInterceptor.java
+++ src/main/java/org/laziji/commons/security/interceptor/CrossDomainInterceptor.java
@@ -5,27 +5,26 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CrossDomainInterceptor implements HandlerInterceptor {
 
-    private static final String TOKEN_KEY = "token";
+    private Set<String> allowMethods = new HashSet<>();
 
-    private Set<RequestMethod> allowMethods = new HashSet<>();
-
-    private String accessControlAllowMethods;
+    private StringBuilder accessControlAllowMethods = new StringBuilder();
 
     private Set<String> allowHeaders = new HashSet<>();
 
-    private String accessControlAllowHeaders;
+    private StringBuilder accessControlAllowHeaders = new StringBuilder();
+
+    private Set<String> exposeHeaders = new HashSet<>();
+
+    private StringBuilder accessControlExposeHeaders = new StringBuilder();
 
     private Integer maxAge = 86400;
 
     private boolean allowCredentials = false;
-
-    private boolean token = false;
 
 
     public CrossDomainInterceptor() {
@@ -38,55 +37,31 @@ public class CrossDomainInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object o) {
         RequestMethod method = RequestMethod.match(request.getMethod());
-        if (method == null || !allowMethods.contains(method)) {
+        if (method == null || !allowMethods.contains(method.name())) {
             return false;
         }
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", accessControlAllowHeaders);
-        response.setHeader("Access-Control-Allow-Methods", accessControlAllowMethods);
+        response.setHeader("Access-Control-Allow-Headers", accessControlAllowHeaders.toString());
+        response.setHeader("Access-Control-Allow-Methods", accessControlAllowMethods.toString());
         if (maxAge != null && maxAge > 0) {
             response.setHeader("Access-Control-Max-Age", maxAge.toString());
         }
         if (allowCredentials) {
             response.setHeader("Access-Control-Allow-Credentials", "true");
         }
-        if (token) {
-            response.setHeader("Access-Control-Expose-Headers", TOKEN_KEY);
-            HttpSession session = request.getSession();
-            response.setHeader(TOKEN_KEY, session.getId());
-        }
         return method != RequestMethod.OPTIONS;
     }
 
     public void addAllowMethod(RequestMethod method) {
-        if (method == null) {
-            return;
-        }
-        if (allowMethods.contains(method)) {
-            return;
-        }
-        allowMethods.add(method);
-        if (accessControlAllowMethods == null) {
-            accessControlAllowMethods = method.toString();
-            return;
-        }
-        accessControlAllowMethods += "," + method.toString();
+        addHeaderValueItem(allowMethods, accessControlAllowMethods, method.name());
     }
 
     public void addAllowHeader(String header) {
-        if (header == null) {
-            return;
-        }
-        header = header.toLowerCase();
-        if (allowHeaders.contains(header)) {
-            return;
-        }
-        allowHeaders.add(header);
-        if (accessControlAllowHeaders == null) {
-            accessControlAllowHeaders = header;
-            return;
-        }
-        accessControlAllowHeaders += "," + header;
+        addHeaderValueItem(allowHeaders, accessControlAllowHeaders, header);
+    }
+
+    public void addExposeHeaders(String header) {
+        addHeaderValueItem(exposeHeaders, accessControlExposeHeaders, header);
     }
 
     public boolean isAllowCredentials() {
@@ -97,19 +72,27 @@ public class CrossDomainInterceptor implements HandlerInterceptor {
         this.allowCredentials = allowCredentials;
     }
 
-    public boolean isToken() {
-        return token;
-    }
-
-    public void setToken(boolean token) {
-        this.token = token;
-    }
-
     public Integer getMaxAge() {
         return maxAge;
     }
 
     public void setMaxAge(Integer maxAge) {
         this.maxAge = maxAge;
+    }
+
+    private void addHeaderValueItem(Set<String> itemSet, StringBuilder headerValue, String item) {
+        if (item == null) {
+            return;
+        }
+        item = item.toLowerCase();
+        if (itemSet.contains(item)) {
+            return;
+        }
+        itemSet.add(item);
+        if (headerValue.length() == 0) {
+            headerValue.append(item);
+            return;
+        }
+        headerValue.append(',').append(item);
     }
 }
